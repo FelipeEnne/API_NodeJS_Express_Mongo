5678fb3da0e6ca608f55878414a51cb913600943
FelipeEnne <felipeenne@gmail.com>
2022-04-14 16:12:12 -0300

    set the middleware

--- src/controllers/authControler.js
+++ src/controllers/authControler.js
@@ -1,8 +1,17 @@
 const express = require("express");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const User = require("../models/user");
+const authConfig = require("../config/auth.json");
 
 const router = express.Router();
+
+function generateToken(params = {}) {
+  return jwt.sign(params, authConfig.secret, {
+    expiresIn: 86400,
+  });
+}
 
 router.post("/register", async (req, res) => {
   const { email } = req.body;
@@ -14,11 +23,26 @@ router.post("/register", async (req, res) => {
 
     user.password = undefined;
 
-    return res.send({ user });
+    return res.send({ user, token: generateToken({ id: user.id }) });
   } catch (err) {
     //return res.status(400).send({ error: "Registration failed" });
     return res.status(400).send(err);
   }
+});
+
+router.post("/authenticate", async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email }).select("+password");
+
+  if (!user) return res.status(440).send({ error: "User not found" });
+
+  if (!(await bcrypt.compare(password, user.password)))
+    return res.status(440).send({ error: "Invalid password" });
+
+  user.password = undefined;
+
+  return res.send({ user, token: generateToken({ id: user.id }) });
 });
 
 module.exports = (app) => app.use("/auth", router);
--- src/index.js
+++ src/index.js
@@ -1,15 +1,35 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
+const Project = require("./models/project");
+const Task = require("./models/task");
+
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get("/", (req, res) => {
-  res.send("OK");
+app.get("/", async (req, res) => {
+  res.send({ user: req.userId });
+});
+
+app.get("/:projectId", async (req, res) => {
+  res.send({ user: req.userId });
+});
+
+app.post("/", async (req, res) => {
+  res.send({ user: req.userId });
+});
+
+app.put("/:projectId", async (req, res) => {
+  res.send({ user: req.userId });
+});
+
+app.delete("/:projectId", async (req, res) => {
+  res.send({ user: req.userId });
 });
 
 require("./controllers/authControler")(app);
+require("./controllers/projectControler")(app);
 
 app.listen(3000);

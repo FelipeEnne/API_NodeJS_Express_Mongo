7dc12c6d7ac9abb745ad42fe89655b2b8292b7de
FelipeEnne <felipeenne@gmail.com>
2022-04-15 13:15:55 -0300

    add CRUD

--- src/index.js
+++ src/index.js
@@ -1,33 +1,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
-const Project = require("./app/models/project");
-const Task = require("./app/models/task");
-
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-
-app.get("/", async (req, res) => {
-  res.send({ user: req.userId });
-});
-
-app.get("/:projectId", async (req, res) => {
-  res.send({ user: req.userId });
-});
-
-app.post("/", async (req, res) => {
-  res.send({ user: req.userId });
-});
-
-app.put("/:projectId", async (req, res) => {
-  res.send({ user: req.userId });
-});
-
-app.delete("/:projectId", async (req, res) => {
-  res.send({ user: req.userId });
-});
 
 require("./app/controllers/index")(app);
 
